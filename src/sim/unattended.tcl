405a8e2872b74e711832ba3c30117cbec4e46f14
rkastl <rkastl@6ca37ae0-1430-4d66-9f89-7499653cdc0f>
2011-01-04 18:44:46 +0000

    Build has to fail on assertions with the severity error as well.

--- src/sim/unattended.tcl
+++ src/sim/unattended.tcl
@@ -2,9 +2,17 @@
 
 # use an error code which is not used by modelsim
 set errcode 88
+set errors 0
 onerror "quit -code $errcode"
+onbreak {incr errors; cont}
 
 # run the script and exit afterwards
 do $script
-exit
+
+if {$errors == 0} {
+	exit
+} else {
+	quit -code $errcode
+}
+
 
