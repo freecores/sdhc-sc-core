df848ceed77b9b6aff6fbe974d00d959d190cc02
rkastl <rkastl@6ca37ae0-1430-4d66-9f89-7499653cdc0f>
2011-01-04 18:49:12 +0000

    Wishbone: reads and writes as procedures in the tb

--- src/grpWishbone/unitWbSlave/src/tbWbSlave-Bhv-ea.vhdl
+++ src/grpWishbone/unitWbSlave/src/tbWbSlave-Bhv-ea.vhdl
@@ -16,18 +16,19 @@ entity tbWbSlave is
 end entity;
 
 architecture Bhv of tbWbSlave is
-
+	subtype aAdr is std_ulogic_vector(7 downto 2);
+	subtype aData is std_ulogic_vector(7 downto 0);
+	
 	signal Clk, RstSync : std_ulogic := cInactivated;
 	signal iWbSlave : aWbSlaveCtrlInput;
 	signal oWbSlave : aWbSlaveCtrlOutput;
 	signal Sel : std_ulogic_vector(0 downto 0);
-	signal Adr : std_ulogic_vector(7 downto 2);
-	signal DataToSlave, DataFromSlave : std_ulogic_vector(7 downto 0);
+	signal Adr : aAdr;
+	signal DataToSlave, DataFromSlave : aData;
 
 	signal Finished : std_ulogic := cInactivated;
 
-	constant cValid : std_ulogic_vector(7 downto 0) := (others =>
-		'1');
+	constant cValid : std_ulogic_vector(7 downto 0) := (others => '1');
 
 begin
 
@@ -37,25 +38,50 @@ begin
 			   cInactivated after 3*gClkPeriod;
 
 	Stimulus : process
+		
+		procedure readData (constant address: in aAdr; variable data : out aData) is
+		begin
+			iWbSlave.Cyc <= cActivated;
+			iWbSlave.Stb <= cActivated;
+			iWbSlave.We <= cInactivated;
+			iWbSlave.Cti <= cCtiClassicCycle;
+			Adr <= address;
+			Sel <= "1";
+			wait until Clk = cActivated;
+
+			wait until Clk = cActivated and oWbSlave.Ack = cActivated;
+			
+			assert (oWbSlave.Ack = cActivated) report 
+			"Read not acknowledged. Waitstate?" severity error;
+			assert (DataFromSlave = cValid) report
+			"Invalid data after read" severity error;
+
+			data := DataFromSlave;
+
+		end procedure readData;
+
+		procedure writeData (constant address: in aAdr; constant data: in aData) is
+			
+		begin
+			iWbSlave.Cyc <= cActivated;
+			iWbSlave.Stb <= cActivated;
+			iWbSlave.We <= cInactivated;
+			iWbSlave.Cti <= cCtiClassicCycle;
+			Adr <= address;
+			Sel <= "1";
+			DataToSlave <= data;
+			wait until Clk = cActivated;
+
+			wait until Clk = cActivated and oWbSlave.Ack = cActivated;
+		end procedure writeData;
+
+		variable tempData : aData;
+
 	begin
 		wait for 6*gClkPeriod;
-	    -- Classic Reads
-		wait until Clk = cActivated;
-		iWbSlave.Cyc <= cActivated;
-		iWbSlave.Stb <= cActivated;
-		iWbSlave.We <= cInactivated;
-		iWbSlave.Cti <= cCtiClassicCycle;
-		Adr <= "000001";
-		Sel <= "1";
-
-		wait until Clk = cActivated;
-		wait until Clk = cActivated;
-		
-		assert (oWbSlave.Ack = cActivated) report 
-		"Read not acknowledged. Waitstate?" severity error;
-		assert (DataFromSlave = cValid) report
-		"Invalid data after read" severity error;
-
+			
+		writeData("000001", X"A0");
+		readData("000001", tempData);
 		wait until Clk = cActivated;
 
 		Finished <= cActivated;
